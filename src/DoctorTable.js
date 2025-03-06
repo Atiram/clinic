5e5d820feeaadfe4c03b1e35f086e418cc92eac6
DESKTOP-7BINQR3\User <marharyta.hrafa@innowise.com>
2025-03-06 15:08:12 +0100

    Remove Unnecessary files

--- src/DoctorTable.js
+++ src/DoctorTable.js
@@ -5,24 +5,24 @@ function DoctorTable() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const url = new URL('https://localhost:7105/Doctor'); // Use URL constructor for easier query parameter handling
+      const url = new URL('https://localhost:7105/Doctor'); 
   
-      // Add query parameters (adjust these as needed)
+      
       url.searchParams.append('IsDescending', false);
       url.searchParams.append('PageNumber', 1);
       url.searchParams.append('PageSize', 5);
       url.searchParams.append('SortParameter', 'LastName'); 
       url.searchParams.append('SortParameter', 'CareerStartYear');
   
-      const response = await fetch(url, { // Use the constructed URL
-        method: 'GET', // Use GET for fetching data
+      const response = await fetch(url, { 
+        method: 'GET', 
         headers: {
-          'accept': 'application/json' // Expect JSON response
+          'accept': 'application/json' 
         }
       });
   
       const data = await response.json();
-      setDoctors(data.results);// Assuming the API returns an array of doctors
+      setDoctors(data.results);
     };
   
     fetchData();
@@ -32,34 +32,30 @@ function DoctorTable() {
     return <div>Loading...</div>;
   }
 
-  return (
+  return (    
     <table>
       <thead>
         <tr>
           <th>Имя</th>
           <th>Фамилия</th>
-          <th>Отчество</th>
-          {/* ... другие заголовки */}
+          <th>Отчество</th>          
         </tr>
       </thead>
       <tbody>
         <tr>
           <td>{doctor[0].firstName}</td>
           <td>{doctor[0].lastName}</td>
-          <td>{doctor[0].middleName}</td>
-          {/* ... другие данные */}
+          <td>{doctor[0].middleName}</td>          
         </tr>
         <tr>
           <td>{doctor[1].firstName}</td>
           <td>{doctor[1].lastName}</td>
-          <td>{doctor[1].middleName}</td>
-          {/* ... другие данные */}
+          <td>{doctor[1].middleName}</td>          
         </tr>
         <tr>
           <td>{doctor[2].firstName}</td>
           <td>{doctor[2].lastName}</td>
-          <td>{doctor[2].middleName}</td>
-          {/* ... другие данные */}
+          <td>{doctor[2].middleName}</td>         
         </tr>
       </tbody>
     </table>
