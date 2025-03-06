e10485870a0c67625aa0f3c2575ba01e7c567935
DESKTOP-7BINQR3\User <marharyta.hrafa@innowise.com>
2025-03-06 12:27:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import DoctorTable from './DoctorTable';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">        
+        <p></p>        
+        <DoctorTable />
+      </header>
+    </div>
+  );
+}
+
+export default App;
